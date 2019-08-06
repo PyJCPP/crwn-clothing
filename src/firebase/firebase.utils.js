4d9e29c47d00dd566d656c58dffc6238ecbfddef
pyjcpp <pyjcpp@gmail.com>
2019-08-06 09:19:09 -0500

    Store user authentication information in database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
   appId: "1:332760201298:web:59c4d9caaa8cf6ab"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+   }
+
+   return userRef;
+};
+
 // Initialize Firebase
 firebase.initializeApp(config);
 
