dace576a28f699926fb75ae92e100d4ab5eaeaa9
pyjcpp <pyjcpp@gmail.com>
2019-08-07 14:00:32 -0500

    Fix typo in css class property on jsx element

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,7 +6,7 @@ import CollectionItem from '../collection-item/collection-item.component';
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
-    <div class="preview">
+    <div className="preview">
       {
         items
           .filter((item, idx) => idx < 4)
